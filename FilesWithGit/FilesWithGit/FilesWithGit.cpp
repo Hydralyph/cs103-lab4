0dc590d383103f151824b522549a0704bdca66e0
Hydralyph <270116757@yoobeestudent.ac.nz>
2021-06-08 20:57:14 +1200

    Iteration 2 - Member 2, added functions for data processing

--- FilesWithGit/FilesWithGit/FilesWithGit.cpp
+++ FilesWithGit/FilesWithGit/FilesWithGit.cpp
@@ -1,3 +1,5 @@
+// CODE SECTION - Member1
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,7 +13,7 @@ struct Jacket {
 	string size;
 	float price;
 
-	Jacket(string br, string mt, string cl, string si, float pr) {
+	Jacket(string br="", string mt="", string cl="", string si="", float pr=0.0) {
 		brand = br;
 		material = mt;
 		colour = cl;
@@ -20,7 +22,66 @@ struct Jacket {
 	}
 };
 
+// CODE SECTION - END
+
+//CODE SECTION - Member2
+void InputData(struct Jacket*, int);
+void WriteData(struct Jacket*, int);
+
+
 int main()
 {
+	const int iterations = 2;	// Controls how many times the loops will run. Make this the size of the array and all for loops
 
+	struct Jacket jackets[iterations];
+	struct Jacket* p_jackets;
+
+	p_jackets = jackets;
+
+	InputData(p_jackets, iterations);
+	WriteData(p_jackets, iterations);
+
+	return 0;
 }
+
+void InputData(struct Jacket* p_jackets, int iterations) {
+
+	for (int i = 0; i < iterations; i++) {
+		cout << "\nINPUT DATA for JACKET " << i + 1 << " (NO SPACES)" << endl;
+	
+		
+		cout << "\nPlease enter the Jacket's Brand: ";
+		cin >> (p_jackets + i)->brand;
+		cout << "Please enter the Jacket's Material: ";
+		cin >> (p_jackets + i)->material;
+		cout << "Please enter the Jacket's Colour: ";
+		cin >> (p_jackets + i)->colour;
+		cout << "Please enter the Jacket's Size: ";
+		cin >> (p_jackets + i)->size;
+		cout << "Please enter the Jacket's Price: ";
+		cin >> (p_jackets + i)->price;
+
+		cout << "\n*********************************\n";
+	}
+}
+
+void WriteData(struct Jacket* p_jackets, int iterations) {
+
+	fstream fs;
+
+	fs.open("jackets.txt", ios::out);
+
+	for (int i = 0; i < iterations; i++) {
+		fs << "#Jacket " << i + 1 << endl;
+		fs << (p_jackets + i)->brand << endl;
+		fs << (p_jackets + i)->material << endl;
+		fs << (p_jackets + i)->colour << endl;
+		fs << (p_jackets + i)->size << endl;
+		fs << (p_jackets + i)->price << endl;
+		fs << "#-------------" << endl;
+	}
+
+	fs.close();
+}
+
+//CODE SECTION - END
