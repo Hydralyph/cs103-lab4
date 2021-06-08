cc6db8b849f99fd4b3e6f72cc8522917e0d03490
Hydralyph <acidicmc@gmail.com>
2021-06-08 14:18:11 +1200

    Interation 1 - New Project commit

---
+++ FilesWithGit/FilesWithGit/FilesWithGit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Jacket {
+	string brand;
+	string material;
+	string colour;
+	string size;
+	float price;
+
+	Jacket(string br, string mt, string cl, string si, float pr) {
+		brand = br;
+		material = mt;
+		colour = cl;
+		size = si;
+		price = pr;
+	}
+};
+
+int main()
+{
+
+}
