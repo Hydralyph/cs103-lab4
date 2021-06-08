2321247efa6a87bd56d14973adc31dd527967681
Hydralyph <acidicmc@gmail.com>
2021-06-08 21:46:25 +1200

    Iteration 3 - Member1 added more functions and data processing

--- FilesWithGit/FilesWithGit/FilesWithGit.cpp
+++ FilesWithGit/FilesWithGit/FilesWithGit.cpp
@@ -27,11 +27,13 @@ struct Jacket {
 //CODE SECTION - Member2
 void InputData(struct Jacket*, int);
 void WriteData(struct Jacket*, int);
+void ReadData(struct Jacket*, int);
+void FilterData(struct Jacket*, int);
 
 
 int main()
 {
-	const int iterations = 2;	// Controls how many times the loops will run. Make this the size of the array and all for loops
+	const int iterations = 5;	// Controls how many times the loops will run. Make this the size of the array and all for loops
 
 	struct Jacket jackets[iterations];
 	struct Jacket* p_jackets;
@@ -40,6 +42,8 @@ int main()
 
 	InputData(p_jackets, iterations);
 	WriteData(p_jackets, iterations);
+	ReadData(p_jackets, iterations);
+	FilterData(p_jackets, iterations);
 
 	return 0;
 }
@@ -67,9 +71,9 @@ void InputData(struct Jacket* p_jackets, int iterations) {
 
 void WriteData(struct Jacket* p_jackets, int iterations) {
 
-	fstream fs;
+	fstream fs;	// I am opening a new fstream each time becuause I could not figure out how to pass fstream variables as parameters
 
-	fs.open("jackets.txt", ios::out);
+	fs.open("Jackets.txt", ios::out);
 
 	for (int i = 0; i < iterations; i++) {
 		fs << "#Jacket " << i + 1 << endl;
@@ -78,10 +82,51 @@ void WriteData(struct Jacket* p_jackets, int iterations) {
 		fs << (p_jackets + i)->colour << endl;
 		fs << (p_jackets + i)->size << endl;
 		fs << (p_jackets + i)->price << endl;
-		fs << "#-------------" << endl;
+		fs << "#-------------\n" << endl;
 	}
 
 	fs.close();
 }
 
 //CODE SECTION - END
+
+//CODE SECTION - Member1
+void ReadData(struct Jacket* p_jackets, int iterations) {
+	string data;
+	
+	fstream fs;
+
+	fs.open("Jackets.txt", ios::in);
+	while (getline(fs, data)) {
+		cout << data << endl;
+	}
+
+	fs.close();
+}
+
+void FilterData(struct Jacket* p_jackets, int iterations) {
+	bool jacketFound = false;
+	string input;
+	cout << "\n\nWhat Size Jacket are you looking for?\nPlease Enter a Size(XS, S, M, L, XL, XXL etc.): ";
+	cin >> input;
+
+	for (int i = 0; i < iterations; i++) {
+		if (input == (p_jackets + i)->size) {
+			cout << "\n\nJacket Found, printing details.\n";
+			cout << "\nJacket Brand: " << (p_jackets + i)->brand;
+			cout << "\nJacket Material: " << (p_jackets + i)->material;
+			cout << "\nJacket Colour: " << (p_jackets + i)->colour;
+			cout << "\nJacket Size: " << (p_jackets + i)->size;
+			cout << "\nJacket Price: " << (p_jackets + i)->price;
+			jacketFound = true;
+		}
+		else if(input != (p_jackets + 1)->size) {
+			cout << "\nSearching...\n";
+		}
+	}
+
+	if (jacketFound == false) {
+		cout << "\nSorry, no Jacket in " << input << " size could be found.";
+	}
+	
+}
